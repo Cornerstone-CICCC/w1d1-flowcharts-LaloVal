b09169b07a11cd124127a793e1d6710d1ae7abfa
LaloVal <eduardov6690@gmail.com>
2024-05-15 17:37:51 -0700

    Answers v2

--- JS/Exercise4.js
+++ JS/Exercise4.js
@@ -1,8 +1,8 @@
-var A= 7
-var B= 10
-var C= 5
+var A= 5
+var B= 15
+var C= 10
 
-if (A+B>=C/B+C>=A/C+A>=B) {
+if (A+B>C && B+C>A && C+A>B) {
     console.log("Triangle is possible")
 } else {
 console.log("Triangle is not possible")}
